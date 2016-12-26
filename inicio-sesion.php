6f155944b15f18d6e75be4ebe0d72d475708f260
Daniela Aliaga <raraliaga@gmail.com>
2016-12-25 22:09:36 -0300

    Añadí archivos base

---
+++ inicio-sesion.php
@@ -0,0 +1,23 @@
+<?php require_once('header-login.php'); ?>
+<section id="login">
+  <h1 class="title">iniciar sesión</h1>
+  <hr class="line-pink">
+  <p class="description">Ingresa tu email y contraseña.</p>
+  <div class="content col-5">
+    <form method="post" action="home-admin.php" id="acceso">
+      <label class="label">email:</label>
+      <input type="email" id="email" value="" placeholder="Ingresa tu email" name="email-access" class="inputs">
+      <label class="label">contraseña:</label>
+      <input type="password" id="pass" value="" placeholder="Ingresa tu contraseña" name="pass-access" class="inputs">
+      <input type="submit" value="iniciar sesión" class="send">
+      <p class="forget"><a href="olvide-pass.php">olvide mi contraseña</a></p>
+      <p class="remember">
+        <input type="checkbox" name="remember" value="recordar">
+        Recuérdame</p>
+    </form>
+  </div>
+  <!-- /content col-6 --> 
+</section>
+<!-- /login -->
+<?php require_once('rrss.php'); ?>
+<?php require_once('footer.php'); ?>
---
+++ reestablecer.php
@@ -0,0 +1,22 @@
+
+<?php require_once('header-login.php'); ?>
+<section id="login">
+  <h1 class="title">reestablecer contraseña</h1>
+  <hr class="line-pink">
+  <p class="description">Ingresa tu email de contacto y confirma tu cambio de contraseña.</p>
+  <div class="content col-5">
+    <form method="post" action="" id="acceso">
+      <label class="label">email:</label>
+      <input type="email" id="email" value="" placeholder="Ingresa tu email" name="email-access" class="inputs">
+      <label class="label">Nueva contraseña:</label>
+      <input type="password" id="pass" value="" placeholder="Ingresa tu nueva contraseña" name="pass-access" class="inputs">
+      <label class="label">Confirma contraseña:</label>
+      <input type="password" id="pass-1" value="" placeholder="Ingresa tu contraseña nuevamente" name="pass-access" class="inputs">
+      <input type="submit" value="reestablecer contraseña" class="send">
+    </form>
+  </div>
+  <!-- /content col-6 --> 
+</section>
+<!-- /login -->
+<?php require_once('rrss.php'); ?>
+<?php require_once('footer.php'); ?>
