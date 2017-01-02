38f8f49149d3fb50e1da0dd02335cb1d0df0349e
Daniela Aliaga <raraliaga@gmail.com>
2017-01-02 01:16:28 -0300

    actualización html

--- home-admin.php
+++ home-admin.php
@@ -4,17 +4,17 @@
   <?php require_once('nav-ppal.php'); ?>
   <section id="index-nav" class="col-8">
     <?php require_once('breadcrumbs.php'); ?>
-      <a href="add-novia.php" class="index dress col-4">
+      <a href="add-novia.php" class="col-4 dress ">
       <h3 class="index-title">añadir novia</h3>
       <p>ingresar nueva novia</p>
       </a> 
       <!-- /col-4 index dress-->
-      <a href="#" class="index size col-4">
+      <a href="#" class="col-4 size">
       <h3 class="index-title">fichas pendientes</h3>
       <p>fichas sin asignar</p>
       </a> 
       <!-- /col-4 index size-->
-       <a href="#" class="index process col-4">
+       <a href="#" class="col-4 process ">
       <h3 class="index-title">buscar</h3>
       <p>realizar busqueda</p>
       </a> 
