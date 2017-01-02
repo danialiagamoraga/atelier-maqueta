d3fd6b28df1decd49a328c658fc16edc56ef67a6
Daniela Aliaga <raraliaga@gmail.com>
2017-01-02 01:21:38 -0300

    actualización html

--- confecciones.php
+++ confecciones.php
@@ -4,12 +4,12 @@
   <?php require_once('nav-ppal.php'); ?>
   <section id="index-nav" class="col-8">
     <?php require_once('breadcrumbs.php'); ?>
-      <a href="confecciones-inside.php" class="index sewing col-4">
+      <a href="confecciones-inside.php" class="col-4 sewing">
       <h3 class="index-title">Lizabeth</h3>
       <p>+</p>
       </a> 
       <!-- /col-4 index dress-->
-      <a href="#" class="index scissors col-4">
+      <a href="#" class="col-4 scissors">
       <h3 class="index-title">añadir modista</h3>
       <p>+</p>
       </a> 
