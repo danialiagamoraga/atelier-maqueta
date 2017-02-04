bd292386bc21f5ca914cef5792d1ab7ebba80cf5
Daniela Aliaga <raraliaga@gmail.com>
2017-02-04 20:51:14 -0300

    Actualización clases

--- header-login.php
+++ header-login.php
@@ -1,25 +1,25 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Iniciar sesión</title>
-<link href="sass/stylesheets/style.css" rel="stylesheet" type="text/css">
-<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <title>Iniciar sesión</title>
+    <link href="stylesheets/style.css" rel="stylesheet" type="text/css">
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <header>
-  <div class="content">
-    <div class="col-4">
-      <h1 class="logo"><a href="#">santo encanto <span>#IniciarSesión</span></a></h1>
+    <div class="content">
+        <div class="col-4">
+            <h1 class="logo"><a href="#">santo encanto <span>#IniciarSesión</span></a></h1>
+        </div>
+        <!--/col-4-->
+        <nav class="col-8">
+            <ul class="list-ppal">
+                <li><a href="https://www.santoencanto.cl/" target="_blank">santoencanto.cl</a></li>
+            </ul>
+        </nav>
+        <!-- /col-8 -->
     </div>
-    <!--/col-4-->
-    <nav class="col-8">
-      <ul class="list-ppal">
-        <li><a href="https://www.santoencanto.cl/" target="_blank">santoencanto.cl</a></li>
-      </ul>
-    </nav>
-    <!-- /col-8 --> 
-  </div>
-  <!-- /content --> 
+    <!-- /content -->
 </header>
